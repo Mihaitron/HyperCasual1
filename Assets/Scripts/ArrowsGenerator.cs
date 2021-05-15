1be792c88b9ed83c9f7d02d5af2dda5ecafef69e
Condor Andrei <44895249+condoran@users.noreply.github.com>
2021-05-16 01:47:08 +0300

    hope it works

--- Assets/Scripts/ArrowsGenerator.cs
+++ Assets/Scripts/ArrowsGenerator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ArrowsGenerator : MonoBehaviour
 {
     [SerializeField] float minTime;
@@ -11,6 +12,13 @@ public class ArrowsGenerator : MonoBehaviour
     [SerializeField] private GameObject leftArrow;
     [SerializeField] private GameObject rightArrow;
     [SerializeField] private GameObject swipePos;
+    [SerializeField] private float perfectDistance;
+    [SerializeField] private float goodDistance;
+    [SerializeField] private float niceDistance;
+    [SerializeField] private ViewsController views;
+    [SerializeField] private int perfectViews;
+    [SerializeField] private int goodViews;
+    [SerializeField] private int niceViews;
 
     private List<GameObject> arrowList = new List<GameObject>();
     private GameObject lastArrow;
@@ -79,23 +87,112 @@ public class ArrowsGenerator : MonoBehaviour
         var dir = lastArrow.GetComponent<ArrowType>().direction;
         if (dir == ArrowDirection.UP && direction == 0)
         {
-            
+            if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < perfectDistance)
+            {
+                // TODO add sound perfect
+                // TODO add particle effect perfect play
+                views.AddViews(perfectViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < goodDistance)
+            {
+                // TODO add sound good job
+                // TODO add particle effect good job play
+                views.AddViews(goodViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < niceViews)
+            {
+                // TODO add sound nice
+                // TODO add particle effect nice play
+                views.AddViews(niceViews);
+            }
+            else
+            {
+                // TODO add sound ohh
+                // TODO add particle effect ohh play
+            }
         }
         else if (dir == ArrowDirection.DOWN && direction == 1)
         {
-            
+            if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < perfectDistance)
+            {
+                // TODO add sound perfect
+                // TODO add particle effect perfect play
+                views.AddViews(perfectViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < goodDistance)
+            {
+                // TODO add sound good job
+                // TODO add particle effect good job play
+                views.AddViews(goodViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < niceViews)
+            {
+                // TODO add sound nice
+                // TODO add particle effect nice play
+                views.AddViews(niceViews);
+            }
+            else
+            {
+                // TODO add sound ohh
+                // TODO add particle effect ohh play
+            }
         }
         else if (dir == ArrowDirection.LEFT && direction == 2)
         {
-            
+            if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < perfectDistance)
+            {
+                // TODO add sound perfect
+                // TODO add particle effect perfect play
+                views.AddViews(perfectViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < goodDistance)
+            {
+                // TODO add sound good job
+                // TODO add particle effect good job play
+                views.AddViews(goodViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < niceViews)
+            {
+                // TODO add sound nice
+                // TODO add particle effect nice play
+                views.AddViews(niceViews);
+            }
+            else
+            {
+                // TODO add sound ohh
+                // TODO add particle effect ohh play
+            }
         }
         else if (dir == ArrowDirection.RIGHT && direction == 3)
         {
-            
+            if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < perfectDistance)
+            {
+                // TODO add sound perfect
+                // TODO add particle effect perfect play
+                views.AddViews(perfectViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < goodDistance)
+            {
+                // TODO add sound good job
+                // TODO add particle effect good job play
+                views.AddViews(goodViews);
+            }
+            else if (Mathf.Abs(lastArrow.transform.position.x - swipePos.transform.position.x) < niceViews)
+            {
+                // TODO add sound nice
+                // TODO add particle effect nice play
+                views.AddViews(niceViews);
+            }
+            else
+            {
+                // TODO add sound ohh
+                // TODO add particle effect ohh play
+            }
         }
         else
         {
-            
+            // TODO add sound ohh
+            // TODO add particle effect ohh play
         }
     }
 }
---
+++ Assets/Scripts/ViewsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ViewsController : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro views;
+
+    private long viewsNumber;
+
+    private void Start()
+    {
+        viewsNumber = 0;
+    }
+
+    public void AddViews(int added)
+    {
+        viewsNumber += added;
+        if (viewsNumber > 999999)
+        {
+            var ks = viewsNumber / 1000000;
+            var dot = viewsNumber / 100000 % 10;
+            views.text = String.Concat(ks.ToString(), ".", dot.ToString(), "M");
+        }
+        else if (viewsNumber > 999)
+        {
+            var ks = viewsNumber / 1000;
+            var dot = viewsNumber /100 % 10;
+            views.text = String.Concat(ks.ToString(), ".", dot.ToString(), "K");
+        }
+        else
+        {
+            views.text = viewsNumber.ToString();
+        }
+    }
+}
