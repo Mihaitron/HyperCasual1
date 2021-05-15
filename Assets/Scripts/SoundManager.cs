a7ff4bbb8ca39de22362a2e59e0f33c8fdfabb38
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-05-16 00:00:19 +0300

    Sound manager added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager
+{
+    private static SoundManager inst;
+
+    public static SoundManager instance
+    {
+        get
+        {
+            if (inst == null)
+            {
+                inst = new SoundManager();
+            }
+
+            return inst;
+        }
+    }
+
+    private AudioClip pressButton = Resources.Load<AudioClip>("Sounds/PressButton");
+
+    public float PlayButtonClickedSound()
+    {
+        GameObject sound_game_object = new GameObject("Sound");
+        AudioSource audio_source = sound_game_object.AddComponent<AudioSource>();
+        
+        audio_source.PlayOneShot(this.pressButton);
+
+        return this.pressButton.length;
+    }
+}
