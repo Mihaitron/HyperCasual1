13108239bcce26b441762980c01dc89e8d23dbc0
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-05-16 13:59:31 +0300

    Player spawns correctly when entering scene

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform spawnPoint;
+    [SerializeField] private bool rotate;
+    [SerializeField] private List<GameObject> skins;
+    private void Start()
+    {
+        int skin_no = PlayerPrefs.GetInt("character_no");
+
+        if (rotate)
+        {
+            Instantiate(skins[skin_no], spawnPoint.position, Quaternion.Euler(new Vector3(0, 180, 0)));
+        }
+        else
+        {
+            Instantiate(skins[skin_no], spawnPoint.position, Quaternion.identity);
+        }
+    }
+}
