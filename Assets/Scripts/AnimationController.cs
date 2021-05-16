49ed818157609efbc933c0c569123136137c26a0
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-05-16 12:21:21 +0300

    Small WIP script for detecting which animation should be played

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    private Animator animator;
+    private int animationNo = 0;
+
+    private void Start()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // do some stuff to check which animation
+        
+        this.animator.SetInteger("anim", this.animationNo);
+    }
+}
