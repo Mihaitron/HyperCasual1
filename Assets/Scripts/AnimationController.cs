978f803954449100de7ef70d4d282f1490dd783f
Mihai Andrei Popa <popamihai18@yahoo.com>
2021-05-16 13:23:12 +0300

    Animation determined random or if awww, that one

--- Assets/Scripts/AnimationController.cs
+++ Assets/Scripts/AnimationController.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AnimationController : MonoBehaviour
 {
     private Animator animator;
-    private int animationNo = 0;
+    [SerializeField] private int animationNo = 0;
 
     private void Start()
     {
@@ -15,7 +16,14 @@ public class AnimationController : MonoBehaviour
 
     private void Update()
     {
-        // do some stuff to check which animation
+        if (false) // Check if "Awww" triggered
+        {
+            
+        }
+        else if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {
+            this.animationNo = Random.Range(2, 4) - 1;
+        }
         
         this.animator.SetInteger("anim", this.animationNo);
     }
